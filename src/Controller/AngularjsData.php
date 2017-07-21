945ef8b60925dd24cbcfc9a789dac6bdef18d630
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-07-21 16:32:15 +0200

    First commit drupal 8

---
+++ src/Controller/AngularjsData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\angular_js_example\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Component\Utility\Html;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Database\Database;
+use Drupal\Core\Database\Query\Condition;
+
+/**
+ * AngularjsData method.
+ *
+ * @package Drupal\angular_js_example\Controller
+ */
+class AngularjsData extends ControllerBase {
+
+
+}
