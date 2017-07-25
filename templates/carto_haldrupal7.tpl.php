1bf2031b48986dbbc78bc88df74875e6f1f4540c
anthonyguiot <guiot1@fst-liec-the-l01.fst.site.univ-lorraine.fr>
2017-07-25 09:37:34 +0200

    Skeleton angular drupal 7

---
+++ templates/carto_haldrupal7.tpl.php
@@ -0,0 +1,20 @@
+<div ng-app="cartoHal">
+        <script type="text/javascript">
+        /* var ConfigWidgetHal={
+            ApiURL:"{{ApiUrl}}",
+            DisplayMap:"{{DisplayMap}}",
+            DisplayDatatable:"{{DisplayTable}}",
+	    DocumentType:"{{document_types}}",
+            query:"{{CollectionName}}"
+          }*/
+  var ConfigWidgetHal={
+            ApiURL:"http://api.archives-ouvertes.fr",
+            DisplayMap:true,
+            DisplayDatatable:true,
+            ResultSize:10000,
+            query:"OTELO-UL"
+          }
+
+        </script>       
+    <search></search>
+</div>
